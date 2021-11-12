d1ed8dbeb8e28efcec50d9466bef2aca42b1c495
Diwash <diwashdahal75@gmail.com>
2021-11-12 10:35:02 +0545

    adds image resize feature

--- sharekit/users/models.py
+++ sharekit/users/models.py
@@ -1,3 +1,4 @@
+from PIL import Image
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -8,3 +9,11 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
