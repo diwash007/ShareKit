7be8eee98c77c3672143428f424c086318a0c208
Diwash <diwashdahal75@gmail.com>
2021-11-14 20:22:08 +0545

    Add: invalid BOID check

--- sharekit/sharekitapp/models.py
+++ sharekit/sharekitapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.aggregates import Max
 from django.db.models.deletion import CASCADE
+from django.core.validators import MaxLengthValidator, MinLengthValidator, RegexValidator
 from django.urls import reverse
 from nepse_func import scrips
 
@@ -17,7 +19,14 @@ class Share(models.Model):
         return reverse('home')
 
 class Demat(models.Model):
-    boid = models.IntegerField()
+    boid = models.IntegerField(validators=[
+        RegexValidator(
+            regex='^1301\d{12}$',
+            message='Invalid BOID',
+            code='invalid_boid'
+            ),
+        ]
+    )
     name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=CASCADE)
 
