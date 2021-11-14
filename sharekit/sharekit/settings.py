c03c8ed75b58ff3e0bfb2c581210d1d1a91c4f6a
Diwash <diwashdahal75@gmail.com>
2021-11-14 22:10:56 +0545

    MODIFY: staticfiles_dirs

--- sharekit/sharekit/settings.py
+++ sharekit/sharekit/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    # BASE_DIR / "static",
     BASE_DIR / 'sharekitapp/static',
     BASE_DIR / 'users/static'
 ]
