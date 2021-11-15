ff3c3c1ca59a36824920cc1c7747abd5b97ddefc
Diwash <diwashdahal75@gmail.com>
2021-11-15 18:24:21 +0545

    FIX: User Profile conflicts

--- users/signals.py
+++ users/signals.py
@@ -4,11 +4,14 @@ from django.dispatch import receiver
 from .models import Profile
 
 
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# @receiver(post_save, sender=User)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+def save_profile(sender, instance, created, **kwargs):
+    user = instance
+    if created:
+        profile = Profile(user=user)
+        profile.save()
