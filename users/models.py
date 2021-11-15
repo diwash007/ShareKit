09729ae9e6176de034a4620d95d425eb34bc3302
Diwash <diwashdahal75@gmail.com>
2021-11-15 16:20:54 +0545

    MODIFY: Profile.save

--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
+    def save(self):
+        super().save()
         img = Image.open(self.image.path)
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
