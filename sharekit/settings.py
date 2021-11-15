cccaa0435d3ed7504caf0db949ec53487e7c562a
Diwash <diwashdahal75@gmail.com>
2021-11-15 15:06:34 +0545

    MODIFY: moves all files to the base directory

--- sharekit/settings.py
+++ sharekit/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.sharekitapp.herokuapp.com']
+ALLOWED_HOSTS = ['www.sharekitapp.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
