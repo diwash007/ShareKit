1f9182c86ce15832faed22348d6c8db9aa8f5cb6
Diwash <diwashdahal75@gmail.com>
2021-11-16 14:01:55 +0545

    YIPPE: static working on production

--- sharekit/settings.py
+++ sharekit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET',"0780et08e07d43kdszf436@glkj4w6")
+SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
