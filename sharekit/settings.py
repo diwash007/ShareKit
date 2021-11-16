4e952f153c33562bd1542ae928e8781d3979887a
Diwash <diwashdahal75@gmail.com>
2021-11-16 11:30:40 +0545

    MODIFY: settings secret

--- sharekit/settings.py
+++ sharekit/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET')
+SECRET_KEY = os.environ.get('SECRET', "lkh627l7jlj2l7jlj4lj7l5j7l5j724lj75lj235lj6l54j7lj257")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
